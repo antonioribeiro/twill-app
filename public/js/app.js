4ed878167cdd7c6c42b8375dc3a74b5d72ea71e6
Antonio Ribeiro <antonio@area17.com>
2020-03-14 18:42:38 +0100

    Add assets

--- public/js/app.js
+++ public/js/app.js
@@ -19287,8 +19287,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/antoniocarlos/code/area17/twill-app/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/antoniocarlos/code/area17/twill-app/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/antonioribeiro/code/twill/twill-app/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/antonioribeiro/code/twill/twill-app/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
